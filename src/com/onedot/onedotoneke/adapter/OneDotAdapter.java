db566f8f7841849e548d3552b929250993b71973
csuhanshuai <1140360615@qq.com>
2015-08-03 11:11:26 +0800

    新增详情一点详情页面

--- src/com/onedot/onedotoneke/adapter/OneDotAdapter.java
+++ src/com/onedot/onedotoneke/adapter/OneDotAdapter.java
@@ -17,17 +17,17 @@ import com.onedot.onedotoneke.api.OneDotAPI.OneDotInfo;
 import com.onedot.onedotoneke.base.BaseAdapter;
 
 public class OneDotAdapter extends BaseAdapter {
-	private List<String> infos;
+//	private List<String> infos;
 	private Context context;
 	
-	public OneDotAdapter(List<String> oneDotInfo,Context context){
-		this.infos = oneDotInfo;
+	public OneDotAdapter(Context context){
+//		this.infos = oneDotInfo;
 		this.context = context;
 	}
 
 	@Override
 	public int getCount() {
-		return infos.size();
+		return 10;
 	}
 
 	@Override
@@ -42,32 +42,36 @@ public class OneDotAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		String info = infos.get(position);
-		ViewHolder viewHolder = null;
-		if(convertView == null) {
-			viewHolder = new ViewHolder();
-			convertView = LayoutInflater.from(context).inflate(R.layout.onedot_item, null);
-			viewHolder.nick_nm = (TextView) convertView.findViewById(R.id.nick_nm);
-			viewHolder.nick_nm.setText("info.getNick_nm()");
-			viewHolder.addr = (TextView) convertView.findViewById(R.id.addr);
-			viewHolder.addr.setText("info.getAddr()");
-			viewHolder.time = (TextView) convertView.findViewById(R.id.time);
-			viewHolder.time.setText("info.getTime()");
-			viewHolder.msg = (TextView) convertView.findViewById(R.id.msg);
-			viewHolder.msg.setText("info.getMsg()");
-		}else {
-			viewHolder = (ViewHolder) convertView.getTag();
-			viewHolder.nick_nm.setText("info.getNick_nm()");
-			viewHolder.addr.setText("info.getAddr()");
-			viewHolder.time.setText("info.getTime()");
-			viewHolder.msg.setText("info.getMsg()");
-		}
-		return convertView;
-	}
-	
-	public final class ViewHolder {
-		public ImageView image;
-		public TextView nick_nm,addr,time,msg;
+//		String info = infos.get(position);
+//		ViewHolder viewHolder = null;
+//		if(convertView == null) {
+//			viewHolder = new ViewHolder();
+//			convertView = LayoutInflater.from(context).inflate(R.layout.onedot_item, null);
+//			viewHolder.nick_nm = (TextView) convertView.findViewById(R.id.nick_nm);
+//			viewHolder.nick_nm.setText("info.getNick_nm()");
+//			viewHolder.addr = (TextView) convertView.findViewById(R.id.addr);
+//			viewHolder.addr.setText("info.getAddr()");
+//			viewHolder.time = (TextView) convertView.findViewById(R.id.time);
+//			viewHolder.time.setText("info.getTime()");
+//			viewHolder.msg = (TextView) convertView.findViewById(R.id.msg);
+//			viewHolder.msg.setText("info.getMsg()");
+//		}else {
+//			viewHolder = (ViewHolder) convertView.getTag();
+//			viewHolder.nick_nm.setText("info.getNick_nm()");
+//			viewHolder.addr.setText("info.getAddr()");
+//			viewHolder.time.setText("info.getTime()");
+//			viewHolder.msg.setText("info.getMsg()");
+//		}
+//		return convertView;
+//	}
+//	
+//	public final class ViewHolder {
+//		public ImageView image;
+//		public TextView nick_nm,addr,time,msg;
+//	}
+		View view = LayoutInflater.from(context).inflate(R.layout.onedot_item,
+				null);
+		return view;
 	}
 
 }
